5092b631a9b39bf08e36ca5854cc96c1f8f4ff15
Griffin Evans <gevans3@sfsu.edu>
2023-10-22 19:49:41 -0700

    main.c attempt to make volume

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
-#include "bfs.h"
+#include "bfs.c"
 
 int main() 
 {
+	struct vcb_s* vcb = malloc(sizeof(struct vcb_s));
+	char* name = "my_volume";
+	if (vcb_init(vcb, name)) {
+		printf("Failed to create volume\n");
+	} else {
+		printf("Sucessfully created volume %s\n", vcb->volume_name);
+	}
+
 	return 0;
 }
