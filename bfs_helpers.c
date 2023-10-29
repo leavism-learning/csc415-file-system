678734f17111d3eccb25d6ac7881e3582dfea2f2
Huy Dang <ghuydang@gmail.com>
2023-10-29 00:28:40 -0700

    Header helper functions

--- bfs_helpers.c
+++ bfs_helpers.c
@@ -1,3 +1,17 @@
+/**************************************************************
+ * Class:  CSC-415-01 Fall 2023
+ * Names: Griffin Evans, Sukrit Dev Dhawan, Michelle Lang, Giahuy Dang
+ * Student IDs: 922498210, 922432027, 917386319, 922722304
+ * GitHub Name: griffinevans
+ * Group Name: Team CDeez
+ * Project: Basic File System
+ *
+ * File: bfs_bitmap.c
+ *
+ * Description: A set of helper functions when working with the
+ * Basic File System (BFS).
+ **************************************************************/
+
 #include "bfs_helpers.h"
 
 int bytes_to_blocks(int bytes, int block_size)
@@ -5,8 +19,8 @@ int bytes_to_blocks(int bytes, int block_size)
 	return (bytes + block_size - 1) / block_size;
 }
 
-void print_dir_entry(struct bfs_dir_entry* bde)
+void print_dir_entry(struct bfs_dir_entry *bde)
 {
-	printf("name: %s  size: %ld  location: %ld  type: %d\n", bde->name, bde->size, 
-			bde->location, bde->file_type);
+	printf("name: %s  size: %ld  location: %ld  type: %d\n", bde->name, bde->size,
+				 bde->location, bde->file_type);
 }
--- bfs_helpers.h
+++ bfs_helpers.h
@@ -1,7 +1,15 @@
-/*
- * BFS helper functions
- * Author: Griffin Evans
- */
+/**************************************************************
+ * Class:  CSC-415-01 Fall 2023
+ * Names: Griffin Evans, Sukrit Dev Dhawan, Michelle Lang, Giahuy Dang
+ * Student IDs: 922498210, 922432027, 917386319, 922722304
+ * GitHub Name: griffinevans
+ * Group Name: Team CDeez
+ * Project: Basic File System
+ *
+ * File: bfs_bitmap.c
+ *
+ * Description: Function definitions for bfs_helpers.
+ **************************************************************/
 #ifndef BFS_HELPERS
 #define BFS_HELPERS
 
@@ -9,6 +17,6 @@
 
 int bytes_to_blocks(int bytes, int block_size);
 
-void print_dir_entry(struct bfs_dir_entry* bde);
+void print_dir_entry(struct bfs_dir_entry *bde);
 
 #endif
