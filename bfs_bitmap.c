29ab945b221fbd2830695741a3ac022671aa64dd
Huy Dang <ghuydang@gmail.com>
2023-10-29 00:25:07 -0700

    Header for bitmap code

--- bfs_bitmap.c
+++ bfs_bitmap.c
@@ -8,27 +8,17 @@
  *
  * File: bfs_bitmap.c
  *
- * Description: Function definitions for bfs_directory
- *
+ * Description: Provides a set of functions to manipulate and query
+ * the bitmap of the Basic File System (BFS).
  **************************************************************/
 
-/*
- * bfs_bitmap.c 
- * functions for working with block group bitmap
- */
-
-/*
- * Get value (0 or 1 of a bit at a certain position in the given byte
- * byte: byte to be read
- * pos: position in byte, 0-7
- */
 #include <bfs_bitmap.h>
 
 // returns 0 if bit is unset, 1 if bit is set
 int bit_check(uint8_t byte, uint8_t pos)
 {
-	return byte & ( 1 << pos );
-	//return (byte >> pos) & 0x1;
+	return byte & (1 << pos);
+	// return (byte >> pos) & 0x1;
 }
 
 /*
@@ -38,14 +28,14 @@ int bit_check(uint8_t byte, uint8_t pos)
 int bit_set(uint8_t byte, uint8_t pos)
 {
 	return byte |= 1 << pos;
-	//return byte | 0x1 << pos;
+	// return byte | 0x1 << pos;
 }
 
 /*
- * Set value of bit at given position in block. 
- * Assumes that pos is valid 
+ * Set value of bit at given position in block.
+ * Assumes that pos is valid
  */
-void block_bit_set(uint8_t* block, uint8_t pos)
+void block_bit_set(uint8_t *block, uint8_t pos)
 {
 	int index = pos % 8;
 
@@ -65,36 +55,19 @@ int bit_toggle(uint8_t byte, uint8_t pos)
 	return byte ^= 1 << pos;
 }
 
-// find empty blocks in bitmap
-// array: buffer to store return values (block #)
-// count: number of blocks to find
-// returns 0 if the operation was sucessful, otherwise 1
-/*
-int get_empty_blocks(uint8_t* array, int count)
-{
-	int blocks_found = 0;
-
-	for (int i = 0; i < block_size * 8; i++) {
-		if (i % 8) {
-			for (int j = 0; j < 8; j++) {
-				if (bit_check == 0)
-			}
-		}
-	}
-	return 1;
-}
-*/
-
 /*
  * Return position of first empty block in bitmap, or -1
  * bitmap: block bitmap
  * size: block size
  */
-int get_empty_block(uint8_t* bitmap, int size)
+int get_empty_block(uint8_t *bitmap, int size)
 {
-	for (int i = 0; i < size; i++) { 
-		for (int j = 0; j < 8; j++) {
-			if (bit_check(bitmap[i], j) == 0) {
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (bit_check(bitmap[i], j) == 0)
+			{
 				return (i * 8) + j;
 			}
 		}
@@ -107,20 +80,22 @@ int get_empty_block(uint8_t* bitmap, int size)
  */
 int bfs_set_block(bfs_block_t block_num)
 {
-	// find out which block group the block belongs to 
+	// find out which block group the block belongs to
 	int block_group = block_num / bfs_vcb->block_group_size;
-	uint8_t* bitmap = malloc(bfs_vcb->block_size);
+	uint8_t *bitmap = malloc(bfs_vcb->block_size);
 
-	if (LBAread(bitmap, 1, bfs_gdt[block_group].bitmap_location) != 1) {
+	if (LBAread(bitmap, 1, bfs_gdt[block_group].bitmap_location) != 1)
+	{
 		fprintf(stderr, "Error: Unable to read bitmap for block %ld\n", block_num);
 		return 1;
 	}
-	
+
 	// index in block group is is block # - ( group size * group # )
 	int block_index = block_num - (bfs_vcb->block_group_size * block_group);
 	block_bit_set(bitmap, block_index);
-	
-	if (LBAwrite(bitmap, 1, bfs_gdt[block_group].bitmap_location != -1)) {
+
+	if (LBAwrite(bitmap, 1, bfs_gdt[block_group].bitmap_location != -1))
+	{
 		fprintf(stderr, "Error: Unable to write to block %ld\n", block_num);
 		return 1;
 	}
@@ -128,23 +103,24 @@ int bfs_set_block(bfs_block_t block_num)
 	free(bitmap);
 
 	return 0;
-
 }
 
 /*
  * Return block num of first available block or -1 and mark that block as used
  */
-int bfs_get_free_block() 
+int bfs_get_free_block()
 {
-	for (int i = 0; i < bfs_vcb->block_group_count; i++) {
+	for (int i = 0; i < bfs_vcb->block_group_count; i++)
+	{
 
 		struct block_group_desc block_group = bfs_gdt[i];
-		
-		// if there are free blocks in the block group, read that block group's 
-		// bitmap and find the first available block
-		if (block_group.free_blocks_count > 0) {
 
-			uint8_t* bitmap = malloc(bfs_vcb->block_size);
+		// if there are free blocks in the block group, read that block group's
+		// bitmap and find the first available block
+		if (block_group.free_blocks_count > 0)
+		{
+
+			uint8_t *bitmap = malloc(bfs_vcb->block_size);
 
 			LBAread(bitmap, 1, block_group.bitmap_location);
 
--- bfs_bitmap.h
+++ bfs_bitmap.h
@@ -1,3 +1,15 @@
+/**************************************************************
+ * Class:  CSC-415-01 Fall 2023
+ * Names: Griffin Evans, Sukrit Dev Dhawan, Michelle Lang, Giahuy Dang
+ * Student IDs: 922498210, 922432027, 917386319, 922722304
+ * GitHub Name: griffinevans
+ * Group Name: Team CDeez
+ * Project: Basic File System
+ *
+ * File: bfs_bitmap.c
+ *
+ * Description: Function definitions for bfs_bitmap.
+ **************************************************************/
 #ifndef BFS_BITMAP
 #define BFS_BITMAP
 
@@ -23,11 +35,11 @@ int bit_clear(uint8_t byte, uint8_t pos);
 
 int bit_toggle(uint8_t byte, uint8_t pos);
 
-int get_empty_block(uint8_t* bitmap, int size);
+int get_empty_block(uint8_t *bitmap, int size);
 
 int bfs_get_free_block();
 
 // set vaule of a bit at given position in block
-void block_bit_set(uint8_t* block, uint8_t pos);
+void block_bit_set(uint8_t *block, uint8_t pos);
 
 #endif
