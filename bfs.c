557d4daaf1590623d5755fe34668a04d4cee1b97
Huy Dang <ghuydang@gmail.com>
2023-10-29 00:32:16 -0700

    Header for bfs

--- bfs.c
+++ bfs.c
@@ -1,22 +1,31 @@
-/*
- * Functions for BFS system.
- * Author: Griffin Evans
- */
-
+/**************************************************************
+ * Class:  CSC-415-01 Fall 2023
+ * Names: Griffin Evans, Sukrit Dev Dhawan, Michelle Lang, Giahuy Dang
+ * Student IDs: 922498210, 922432027, 917386319, 922722304
+ * GitHub Name: griffinevans
+ * Group Name: Team CDeez
+ * Project: Basic File System
+ *
+ * File: bfs_bitmap.c
+ *
+ * Description: Implementation of core functionality for the Basic File
+ * System (BFS), including volume initialization, group descriptor
+ * management, and directory entry operations.
+ **************************************************************/
 #include "bfs.h"
 
 /*
  * Creates a volume with the given name. Returns 0 on success, non-zero on failure
  */
-int bfs_vcb_init(char* name, uint64_t num_blocks, uint64_t block_size) 
+int bfs_vcb_init(char *name, uint64_t num_blocks, uint64_t block_size)
 {
-	bfs_vcb->block_size   	= block_size;
-	bfs_vcb->block_count  	= num_blocks;
-	bfs_vcb->magic 	  	= 0x4465657A;
-	bfs_vcb->block_group_size 	= block_size * 8;
-	
-	bfs_vcb->block_group_count  = num_blocks / bfs_vcb->block_group_size;
-	if (num_blocks % bfs_vcb->block_group_size) 
+	bfs_vcb->block_size = block_size;
+	bfs_vcb->block_count = num_blocks;
+	bfs_vcb->magic = 0x4465657A;
+	bfs_vcb->block_group_size = block_size * 8;
+
+	bfs_vcb->block_group_count = num_blocks / bfs_vcb->block_group_size;
+	if (num_blocks % bfs_vcb->block_group_size)
 		bfs_vcb->block_group_count++;
 
 	int gdt_bytes = bfs_vcb->block_group_count * sizeof(struct block_group_desc);
@@ -36,14 +45,15 @@ int bfs_vcb_init(char* name, uint64_t num_blocks, uint64_t block_size)
  * Group descriptor table contains block_size / sizeof(block_group_desc) block groups
  * Each block group contains block_size * 8 blocks.
  *
- * uint8_t* gdt: buffer to store gdt data  
+ * uint8_t* gdt: buffer to store gdt data
  */
-int bfs_gdt_init(struct block_group_desc* gdt) 
+int bfs_gdt_init(struct block_group_desc *gdt)
 {
 	// bitmap location is directly after the gdt
-	int bitmap_pos = bfs_vcb->gdt_size + 1; 
+	int bitmap_pos = bfs_vcb->gdt_size + 1;
 
-	for (int i = 0; i < bfs_vcb->block_group_count; i++) {
+	for (int i = 0; i < bfs_vcb->block_group_count; i++)
+	{
 
 		// create block group descriptor
 		struct block_group_desc descriptor;
@@ -51,15 +61,16 @@ int bfs_gdt_init(struct block_group_desc* gdt)
 		descriptor.free_blocks_count = bfs_vcb->block_group_size - 1;
 		descriptor.dirs_count = 0;
 
-		uint8_t* bitmap = calloc(bfs_vcb->block_size, 1);
+		uint8_t *bitmap = calloc(bfs_vcb->block_size, 1);
 
 		// set 1st block of group because it contains the bitmap
 		block_bit_set(bitmap, 0);
 
-		// write bitmap to disk 
-		if (LBAwrite(bitmap, 1, bitmap_pos) != 1) {
-			fprintf(stderr, "Error: Unable to LBAwrite bitmap %d to disk\n", 
-					bitmap_pos);
+		// write bitmap to disk
+		if (LBAwrite(bitmap, 1, bitmap_pos) != 1)
+		{
+			fprintf(stderr, "Error: Unable to LBAwrite bitmap %d to disk\n",
+							bitmap_pos);
 			return 1;
 		}
 
@@ -71,35 +82,33 @@ int bfs_gdt_init(struct block_group_desc* gdt)
 	return 0;
 }
 
-
 /*
- * Initialize a directory entry. 
+ * Initialize a directory entry.
  */
-int create_dir_entry(struct bfs_dir_entry* dentry, char* name, int size, int type) 
+int create_dir_entry(struct bfs_dir_entry *dentry, char *name, int size, int type)
 {
-	//dentry->vcb = (uint64_t) &vcb;
+	// dentry->vcb = (uint64_t) &vcb;
 	dentry->size = size;
 	dentry->file_type = type;
-
 
 	time_t current_time = time(NULL);
 	dentry->date_created = current_time;
 	dentry->date_modified = current_time;
 	dentry->date_accessed = current_time;
 
-	if (strlen(name) > MAX_FILENAME_LEN - 1) {
+	if (strlen(name) > MAX_FILENAME_LEN - 1)
+	{
 		return 1;
 	}
 	strcpy(dentry->name, name);
 
-
 	return 0;
 }
 
-void bfs_generate_uuid(uint8_t* uuid)
+void bfs_generate_uuid(uint8_t *uuid)
 {
 	// fill buffer with random values
-	//get_random_bytes(uuid, 16);
+	// get_random_bytes(uuid, 16);
 	getrandom(uuid, 16, 0);
 
 	// set the four most significant bits of the 7th byte to 0100
@@ -113,6 +122,4 @@ void bfs_generate_uuid(uint8_t* uuid)
 
 int init_directory(int is_root)
 {
-
 }
-
--- bfs.h
+++ bfs.h
@@ -1,5 +1,15 @@
-/*
- * Function definitions for BFS system Author: Griffin Evans */
+/**************************************************************
+ * Class:  CSC-415-01 Fall 2023
+ * Names: Griffin Evans, Sukrit Dev Dhawan, Michelle Lang, Giahuy Dang
+ * Student IDs: 922498210, 922432027, 917386319, 922722304
+ * GitHub Name: griffinevans
+ * Group Name: Team CDeez
+ * Project: Basic File System
+ *
+ * File: bfs_bitmap.c
+ *
+ * Description: Function definition for bfs.
+ **************************************************************/
 #ifndef BFS_H
 #define BFS_H
 
@@ -14,22 +24,22 @@
 /*
  * Creates a volume with the given name. Returns 0 on success, non-zero on failure
  */
-int bfs_vcb_init(char* name, uint64_t num_blocks, uint64_t block_size);
+int bfs_vcb_init(char *name, uint64_t num_blocks, uint64_t block_size);
 
 /*
  * Create the group descriptor table
  */
-int bfs_gdt_init(struct block_group_desc* gdt);
+int bfs_gdt_init(struct block_group_desc *gdt);
 
 /*
  * Initialize a directory entry. Returns 0 on success, non-zero on failure
  */
-int create_dir_entry(struct bfs_dir_entry* dentry, char* name, int size, int type);
+int create_dir_entry(struct bfs_dir_entry *dentry, char *name, int size, int type);
 
 /*
  * Generate a random UUID. Returns 0 on success, non-zero on failure
  */
-void bfs_generate_uuid(uint8_t* uuid);
+void bfs_generate_uuid(uint8_t *uuid);
 
 /*
  * Get value of bit at given position
@@ -38,8 +48,8 @@ uint8_t get_bit_value(uint8_t byte, uint8_t pos);
 
 int bfs_get_free_block();
 
-int is_valid_volname(char* name);
+int is_valid_volname(char *name);
 
-void print_uuid(uint8_t* uuid);
+void print_uuid(uint8_t *uuid);
 
 #endif
