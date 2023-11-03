5c71c50674743edfabf757a2974fe1dd7d943026
Student <michellelang787@gmail.com>
2023-11-02 23:49:25 -0700

    added fs_stat.c, still WIP

---
+++ fs_stat.c
@@ -0,0 +1,45 @@
+// Separate files for holding functions and helper functions of
+// fs_stat for now
+// Integrate into the rest of the code later
+
+
+
+#include "mfs.h"
+
+// Here for now to make sure function works
+// TODO: delete later
+#define B_CHUNK_SIZE 512
+
+// TODO: delete later
+// for reference
+/*struct bfs_dir_entry {
+  uint64_t size;               // file size in bytes (max 16384 PiB)
+  bfs_block_t location;        // lba position of file extents
+  uint8_t file_type;           // 0 if directory, otherwise file
+  time_t date_created;         // file creation time
+  time_t date_modified;        // last time file was modified
+  time_t date_accessed;        // last time file was read
+  char name[MAX_FILENAME_LEN]; // file name
+};
+// This is the strucutre that is filled in from a call to fs_stat
+struct fs_stat {
+  off_t st_size;         total size, in bytes 
+  blksize_t st_blksize; /* blocksize for file system I/O 
+  blkcnt_t st_blocks;   /* number of 512B blocks allocated 
+  time_t st_accesstime; /* time of last access 
+  time_t st_modtime;    /* time of last modification 
+  time_t st_createtime; /* time of last status change 
+};
+
+*/
+
+int fs_stat(const char *path, struct fs_stat *buf) {
+  struct bfs_dir_entry* dir_entry;
+  get_file_from_path(dir_entry, path);
+  buf->st_blksize = B_CHUNK_SIZE;
+	buf->st_accesstime = dir_entry->date_accessed;
+	buf->st_size = dir_entry->size;
+	buf->st_createtime = dir_entry->date_created;
+	buf->st_modtime = dir_entry->date_modified;
+	//for st_blocks -> use new len field
+}
