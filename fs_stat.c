c06fe462b8ddd760ec9c477946e20327b5b8b7c7
Student <michellelang787@gmail.com>
2023-11-03 12:15:17 -0700

    changed return value

--- fs_stat.c
+++ fs_stat.c
@@ -29,17 +29,22 @@ struct fs_stat {
   time_t st_modtime;    /* time of last modification 
   time_t st_createtime; /* time of last status change 
 };
-
 */
 
+// return 1 if failed to get file from path
 int fs_stat(const char *path, struct fs_stat *buf) {
   // TODO: test to make sure this works
-  struct bfs_dir_entry* dir_entry;
-  get_file_from_path(dir_entry, path);
+  struct bfs_dir_entry* dir_entry = malloc(sizeof(struct bfs_dir_entry));
+  if (!get_file_from_path(dir_entry, path))
+  {
+    return 1;
+  }
   buf->st_blksize = MINBLOCKSIZE;
 	buf->st_accesstime = dir_entry->date_accessed;
 	buf->st_size = dir_entry->size;
 	buf->st_createtime = dir_entry->date_created;
 	buf->st_modtime = dir_entry->date_modified;
   buf->st_blocks = bytes_to_blocks(dir_entry->size);
+  free(dir_entry);
+  // TODO: check if function also has to print fs_stat struct
 }
