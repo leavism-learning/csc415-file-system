eadc8d833666f576983138d2e73c77c6cb84c27b
parallels <sdhawan@sfsu.edu>
2023-11-09 01:18:21 -0800

    Can get the parent path. Still WIP

--- fs_mkdir.c
+++ fs_mkdir.c
@@ -4,24 +4,27 @@
 
 int fs_mkdir(const char *pathname, mode_t mode)
 {
-	struct bfs_dir_entry parent;
+	struct bfs_dir_entry* parent = malloc(sizeof(struct bfs_dir_entry));
 	struct bfs_dir_entry dentry;
 	char* lastElement;
 	//the whole path until the new dir to make
 	char* parentPath = "";
 	char* delim = "/";
 
-	char* copy_pathname = (char*) pathname;
+	char* copy_pathname = strdup(pathname);
 
 	//for relative path
 	if(copy_pathname[0] != '/') {
-		parent = *bfs_cwd;
+		parent = bfs_cwd;
 		lastElement = copy_pathname;
 	}
 	//for absolute path
 	else {
+		int pathLength = strlen(copy_pathname);
+		parentPath = strdup(copy_pathname);
 		char* token1 = strtok(copy_pathname, delim);
 		char* token2;
+		printf("ParentPath: %s\n", parentPath);
 
 		while(token1 != NULL) {
 			token2 = strtok(NULL, delim);
@@ -29,14 +32,18 @@ int fs_mkdir(const char *pathname, mode_t mode)
 				lastElement = token1;
 				break;
 			}
-			strcat(parentPath, delim);
-			strcat(parentPath, token1);
 			token1 = token2;
 		}
 
-		if(get_file_from_path(&parent, parentPath) != 0) {
+		printf("Size of last element: %d			%s\n", strlen(lastElement), lastElement);
+
+		parentPath[pathLength - strlen(lastElement)] = '\0';
+		
+
+		if(get_file_from_path(parent, parentPath) != 0) {
 			fprintf(stderr, "Incorrect parent path!");
 		}
+		// printf("ParentPath: %s, 		Parent DE name: %s\n", parentPath, parent->name);
 
 	}
 
@@ -44,9 +51,14 @@ int fs_mkdir(const char *pathname, mode_t mode)
 		fprintf(stderr, "Name provided is too long!");
 	}
 
+	// bfs_create_file()
+
+	//parent[] = dentry
+
 	//TODO: have to finish and use bfs_create_directory
 
 	dentry.location = bfs_get_free_block();
-	bfs_create_directory(dentry.location, parent.location);
+	bfs_create_directory(dentry.location, parent->location);
 
+	free(parent);
 }
