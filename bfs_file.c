60d230167c0ccf4e1d68811162b70ca53e82f556
Griffin Evans <gevans3@sfsu.edu>
2023-11-27 15:45:19 -0800

    move delete mkdir and stat into bfs_directory and bfs_file

--- bfs_file.c
+++ bfs_file.c
@@ -124,3 +124,89 @@ int bfs_read_extent(void* data, bfs_block_t block_num)
 
 	return 0;
 }
+
+// recursive function to free extent memory
+int free_extents(uint8_t* buffer) {
+	struct bfs_extent_header header = ((struct bfs_extent_header*) buffer)[0];
+
+	// if depth is 0, then the next section is a leaf node which points
+	// to blocks that should be cleared
+	if (header.eh_depth == 0) {
+		for (int i = 0; i < header.eh_entries; i++) {
+			// offset by 1 to skip header
+			struct bfs_extent leaf = ((struct bfs_extent*) buffer)[1 + i];  
+			bfs_clear_blocks(leaf.ext_block, leaf.ext_len);
+			return 0;
+		}
+	}
+	else {
+		for (int i = 0; i < header.eh_entries; i++) {
+			struct bfs_extent_idx idx = ((struct bfs_extent_idx*) buffer)[1];
+			uint8_t* newbuf = malloc(bfs_vcb->block_size);
+
+			if (LBAread(newbuf, 1, idx.idx_leaf) != 1) {
+				fprintf(stderr, "unable to read extent block %ld\n", idx.idx_leaf);
+				free(newbuf);
+				return 1;
+			}
+
+			free_extents(newbuf);
+			free(newbuf);
+		}
+	}
+	return 0;
+}
+
+int fs_delete(char* filename) {
+	struct bfs_dir_entry file;
+	if (get_file_from_path(&file, filename)) {
+		fprintf(stderr, "Unable to find file %s\n", filename);
+		return 1;
+	}
+
+	if (file.file_type == 0) {
+		fprintf(stderr, "Error: %s is a directory\n", filename);
+		return 1;
+	}
+
+	// read extents to find which blocks must be freed
+	uint8_t* buffer = malloc(bfs_vcb->block_size);
+
+	if (LBAread(buffer, 1, file.location) != 1) {
+		fprintf(stderr, "Unable to LBAread block %ld in fs_delete\n", file.location);
+		free(buffer);
+		return 1;
+	}
+
+	if (free_extents(buffer)) {
+		free(buffer);
+		return 1;
+	}
+
+	free(buffer);
+	buffer = NULL;
+
+	// TODO remove directory entry 
+	
+	return 0;
+}
+
+// return 1 if failed to get file from path
+int fs_stat(const char* path, struct fs_stat* buf) 
+{
+	// TODO: test to make sure this works
+	struct bfs_dir_entry dir_entry;
+	// TODO: ask about const char
+	const char* copy_path = strdup(path); 
+	if (!get_file_from_path(&dir_entry, (char*)copy_path))
+	{
+		return 1;
+	}
+	buf->st_blksize = bfs_vcb->block_size;
+	buf->st_accesstime = dir_entry.date_accessed;
+	buf->st_size = dir_entry.size;
+	buf->st_createtime = dir_entry.date_created;
+	buf->st_modtime = dir_entry.date_modified;
+	buf->st_blocks = bytes_to_blocks(dir_entry.size);
+	return 0;
+}
